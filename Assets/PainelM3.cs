e6fac59a76d0f5a9edd049844ae56d1f4650d670
luisfern4ndo <luis-100fernando@hotmail.com>
2019-10-19 18:17:02 -0300

    input variavel de movimentação plataforma

---
+++ Assets/PainelM3.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PainelM3 : MonoBehaviour
+{
+    public GameObject painelM3;
+    public bool colidPlayer;
+    public bool painelM3Ativo;
+    public string texto;
+    public TMP_InputField textoinput;   
+
+    public Text aperteX;
+
+
+    void Start()
+    {
+        aperteX.enabled = false;
+        
+    }
+
+
+    public void Update()
+    {
+        texto = textoinput.text;
+
+        if (Input.GetKeyDown(KeyCode.X) && colidPlayer && !painelM3Ativo)
+        {
+            ativarP3();
+            aperteX.enabled = false;
+
+        }
+
+        else if (!colidPlayer || Input.GetKeyDown(KeyCode.X) && painelM3Ativo)
+        {
+            P3Continuar();
+
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.KeypadEnter) && painelM3Ativo)
+        {
+            P3Continuar();
+
+        }
+
+
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision2d)
+    {
+        if (collision2d.gameObject.CompareTag("Player"))
+        {
+            colidPlayer = true;
+            aperteX.enabled = true;
+
+        }
+
+    }
+
+    public void OnTriggerExit2D(Collider2D collision2d)
+    {
+        if (collision2d.gameObject.CompareTag("Player"))
+        {
+            colidPlayer = false;
+            aperteX.enabled = false;
+        }
+
+    }
+
+
+    public void ativarP3()
+    {
+        painelM3.SetActive(true);
+        painelM3Ativo = true;
+
+    }
+    public void P3Continuar()
+    {
+        painelM3.SetActive(false);
+        painelM3Ativo = false;
+
+    }
+
+    
+    public void ativarPlataforma()
+    {
+        if (texto == "true" || texto == "True") {
+            AtivarMoveP.ativar = true;
+        }
+        else
+        {
+            Debug.Log("ERRADO!");
+        }
+    }
+
+
+}
