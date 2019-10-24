d90f04c61dfb7ffa0cbfe940e841f51485d40277
luisfern4ndo <luis-100fernando@hotmail.com>
2019-10-24 00:24:37 -0300

    Nova etapa if else para os lasers

---
+++ Assets/Scripts/LaserAzul.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserAzul : MonoBehaviour
+{
+    public float timer = 0.0f;
+    public float waitingTime = 2.0f;
+    public GameObject laserVerde;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+
+        timer += Time.deltaTime;
+        if (timer > waitingTime)
+        {
+            laserVerde.SetActive(true);
+            gameObject.SetActive(false);
+            timer = 0f;
+        }
+    }  
+}
---
+++ Assets/Scripts/LaserVerde.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserVerde : MonoBehaviour
+{ 
+public float timer = 0.0f;
+public float waitingTime = 2.0f;
+public GameObject laserAzul;
+void Start()
+{
+
+}
+void Update()
+{
+
+    timer += Time.deltaTime;
+    if (timer > waitingTime)
+    {
+        laserAzul.SetActive(true);
+        gameObject.SetActive(false);
+            timer = 0f;
+    }
+}  
+}
