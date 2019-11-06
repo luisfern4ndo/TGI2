32f29d278eba73f40597ce37071f8278c8373063
luisfern4ndo <luis-100fernando@hotmail.com>
2019-11-06 05:05:11 -0300

    Resolução e Hiperlink

---
+++ Assets/Scripts/hiperlink.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hiperlink : MonoBehaviour
+{
+    
+    public void openForm()
+    {
+        Application.OpenURL("https://docs.google.com/forms/d/1ft2f70ttvA77kuh3e9QTKZ_OfyihmmguVE0N_Exar-w/edit");
+    }
+
+}
