34738f0967b31a4f63c374610a6cccc560c39da8
luisfern4ndo <luis-100fernando@hotmail.com>
2019-11-11 03:38:10 -0300

    Att questionário e cenário

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public static AudioManager instance;
+   // public float highPitchRange = 1.05f;
+   // public float lowPitchRange = 95f;
+
+
+    void Awake()
+    {
+        if (instance == null){
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+   // public void RandomPitch()
+  //  {
+  //      float randomPitch = Random.Range(lowPitchRange, highPitchRange);
+   //     gameObject.GetComponent<AudioSource>().pitch = randomPitch;
+  //  }
+
+}
